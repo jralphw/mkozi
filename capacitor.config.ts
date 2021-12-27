04274bd623225e5dd138217326a2bfed1235e241
jralphw <jonathanwandera@gmail.com>
2021-12-27 17:59:22 +0300

    added capapcitor android dependencies

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'tk.jralphw.mkozi',
   appName: 'mkozi',
   webDir: 'www',
   bundledWebRuntime: false
